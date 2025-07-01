37c6a0dac8cc0cfa3adfc8c04d4e90c20531021d
NallamoluNagaDivya <nallamolusrisanthi@gmail.com>
2025-07-01 13:57:41 +0530

    Add files via upload

---
+++ project files/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-center">
+      {/* Centered blank navbar or with a title */}
+    </nav>
+  );
+}
